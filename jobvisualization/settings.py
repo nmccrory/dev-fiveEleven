4a261cfc95e18b86628ee02ca4938b06209fd66f
Nick McCrory <n.mccrory@icloud.com>
2015-08-01 23:43:24 -0700

    Added a static ROOT to settings.py

--- jobvisualization/settings.py
+++ jobvisualization/settings.py
@@ -112,7 +112,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-# STATIC_ROOT = 'staticfiles'
+STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
